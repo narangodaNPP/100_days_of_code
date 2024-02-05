51fd0cc51955f7e44959e1d3fbd63854505e2de8
naran <narangodapasan@gmail.com>
2024-02-05 21:20:23 +0530

    Day 03

---
+++ day_03/day-3-end.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+  print("You're eligible to ride rollercoster!")
+else:
+  print("You're not eligible to ride rollercoster!")
