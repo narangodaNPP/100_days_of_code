92f07a06d24c8a596b344dd374c63405a65bbdeb
naran <narangodapasan@gmail.com>
2024-02-05 21:13:27 +0530

    Day 01

---
+++ day_01/band-name-generator-end.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print("Hello user!")
+#2. Ask the user for the city that they grew up in.
+city = input("What is the city you grew up? \n")
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of your pet? \n")
+#4. Combine the name of their city and pet and show them their band name.
+print(f"Your band name is {city} {pet}")
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
---
+++ day_01/day-1-printing-end.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+
+print("Hello World!\n" * 3)
+
+name = "Pasan"
+print(f"Hello {name}")
+
+# name = input("What is your name? ")
+print(f"Hello {input('What is your name? ')}")
